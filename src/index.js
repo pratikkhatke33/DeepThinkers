c248c29cae4facf9ffafe9dfb4f45b1580d3ae4f
SnehalMane008 <snehalmane8604@gmail.com>
2026-02-28 02:51:39 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SafeTap from './SafeTap';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SafeTap />
+  </React.StrictMode>
+);
